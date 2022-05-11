963f178de850a41a465b29c3faa238be815f977a
Sebastien Klasa <klasa.sebastien@gmail.com>
2022-05-11 20:49:23 +0000

    split e2e tests into function and classes tests

--- test/e2e/python_unit_tests/test_functions.py
+++ test/e2e/python_unit_tests/test_functions.py
@@ -10,15 +10,3 @@ def test_string_return_value_can_be_used():
 
 def test_bound_function_can_take_parameters():
     assert bindings.add(1, 2) == 3
-
-def test_classes_are_bound():
-    """Constructing a C++ class should not throw if it is bound"""
-    world = bindings.World()
-    assert world is not None
-
-def test_methods_are_bound():
-    world = bindings.World()
-    world.set('Hello world!')
-    assert world.greet() == 'Hello world!'
-
-# TODO: struct
